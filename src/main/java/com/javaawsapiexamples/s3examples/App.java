6c4231ceea40304eb2887bd899ed83f1b67c8793
Alex-Burgess <burgess.alexander@gmail.com>
2018-05-31 15:16:44 +0100

    Empty maven project

---
+++ src/main/java/com/javaawsapiexamples/s3examples/App.java
@@ -0,0 +1,13 @@
+package com.javaawsapiexamples.s3examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
